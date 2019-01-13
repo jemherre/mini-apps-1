d742dfb6a831fb2cf6736abff109463a264916c9
Jessica <jessica.herrera82094@gmail.com>
2019-01-13 00:59:27 -0800

    refactored to single page app, ajax&jquery implementation

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,19 +1,48 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-// var library = require('./helperFunc');
 
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-app.post('/getCSV', function(req, res){
-    //make sure data has no semicolon
-    var keyColumn = {};
-    var result = library.csvParse(data, [],keyColumn);
-    var csvData = Object.keys(keyColumn).join() + '\n' + result;
-    res.send(`${library.renderForm()}<br><p>${csvData}</p>`);
-    res.end();
-  });
+var csvParse = function (obj, arr, keyCol){
+    var result = '';
+    for(var key in obj){
+        if(!keyCol[key] & (key != 'children')) { //add new key into key column list
+            keyCol[key] = key; 
+        } 
+        if(key !== 'children'){
+            arr.push(obj[key]);
+        } else {
+            result = arr.join() + '\n';
+            for(var i = 0; i< obj[key].length; i++){//iterate through children recursion
+                result += csvParse(obj[key][i], [], keyCol);
+            }
+        }
+    }
+    if(result === ''){
+        result = arr.join() + '\n';
+    }
+    return result;//return stringified csv data
+};
+
+// app.post('/getCSV', function(req, res){
+//     //make sure data has no semicolon
+//     var keyColumn = {};
+//     var result = csvParse(data, [], keyColumn);
+//     var csvData = Object.keys(keyColumn).join() + '\n' + result;
+//     res.send(`${renderForm()}<br><p>${csvData}</p>`);
+//   });
+
+app.post('/upload_json', function(req, res){
+     var data = JSON.parse(req.body.fileData.split(';')[0]);
+    var keyList = {};
+    var result = csvParse(data, [], keyList);
+    var csvData = Object.keys(keyList).join() + '\n' + result;
+    console.log(csvData);
+    res.send(`<p>${csvData}</p>`);
+});
+
 
 module.exports = app;
--- challenge_2/client/helperFunc.js
+++ challenge_2/client/helperFunc.js
@@ -1,56 +1,53 @@
-
-var renderForm = function(){
-    return `<form action='/getCSV' method='post'>
-            <input type="text" name="textarea">
-            <input type="submit" value="Submit">
-    </form>`;
-};
-
-var csvParse = function (obj, arr, keyCol){
-    var result = '';
-    for(var key in obj){
-        if(!keyCol[key] & (key != 'children')) { //add new key into key column list
-            keyCol[key] = key; 
-        } 
-        if(key !== 'children'){
-            arr.push(obj[key]);
-        } else {
-            result = arr.join() + '\n';
-            for(var i = 0; i< obj[key].length; i++){
-                result += csvParse(obj[key][i], [], keyCol);
+$(document).ready(function(){
+    $('form').on('submit', function(event){
+        event.preventDefault();
+        var reader = new FileReader();
+        var fileInput = document.getElementById('inputFile').files;
+        if(fileInput.length !== 0){
+            reader.onload = function () {
+                var fileData = reader.result;
+                console.log(fileData);
+                $.ajax({
+                    method: 'POST', 
+                    url: '/upload_json',
+                    data: {fileData},
+                    success: function(result){
+                        console.log(result);
+                        //render html page here
+                        $('#csvTemplate').html('');//clear before appending
+                        $('#csvTemplate').append(result);
+                    }
+                });
             }
+            reader.readAsBinaryString(fileInput[0]);
         }
-    }
-    if(result === ''){
-        result = arr.join() + '\n';
-    }
-    return result;//return stringified array
-};
 
-var openFile = function(event){
-    var reader = new FileReader();
-    var fileInput = event.target;
-    //check if file was selected
-    if(fileInput.files.length !== 0){
-        reader.onload = function () {
-            var buffer = reader.result;
-            var data = JSON.parse(buffer.split(';')[0]);
-            var keyList = {};
-            var result = csvParse(data, [], keyList);
-            var csvData = Object.keys(keyList).join() + '\n' + result;
-            console.log(csvData);
-            document.getElementById('form').innerHTML = `
-            <form>
-                <input type="file" onchange='openFile(event)'>
-            </form>
-            <br><p>${csvData}</p>`;
-        }
-        reader.readAsBinaryString(fileInput.files[0]);
-    } else {
+    });
 
-    }
-};
+});
 
+// var openFile = function(event){
+//     var reader = new FileReader();
+//     var fileInput = event.target;
+//     //check if file was selected
+//     if(fileInput.files.length !== 0){
+//         reader.onload = function () {
+//             var buffer = reader.result;
+//             var data = JSON.parse(buffer.split(';')[0]);
+//             var keyList = {};
+//             var result = csvParse(data, [], keyList);
+//             var csvData = Object.keys(keyList).join() + '\n' + result;
+//             console.log(csvData);
+//             document.getElementById('form').innerHTML = `
+//             <form>
+//                 <input type="file" onchange='openFile(event)'>
+//             </form>
+//             <br><p>${csvData}</p>`;
+//         }
+//         reader.readAsBinaryString(fileInput.files[0]);
+//     } else {
 
+//     }
+// };
 
-module.exports = {openFile, csvParse, renderForm};
+// module.exports = {csvParse, renderForm};
