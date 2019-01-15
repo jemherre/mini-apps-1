472a1c67dc05ba439a72004c8237373cccea7e98
Jessica <jessica.herrera82094@gmail.com>
2019-01-15 15:19:00 -0800

    throw changes

--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,26 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.post('/create_account',function(req, res){
+
+});
+
+app.post('/shipping_info',function(req, res){
+
+});
+
+app.post('/billing_info',function(req, res){
+
+});
+
+app.get('/confirmation',function(req, res){
+
+});
+
+app.listen(3000, function(){
+    console.log('Listening on port: 3000!');
+});
