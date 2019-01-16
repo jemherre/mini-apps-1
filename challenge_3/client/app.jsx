eb8b6bff2a342933f6dfdf103c7019f45140f6c2
Jessica <jessica.herrera82094@gmail.com>
2019-01-15 20:42:36 -0800

    configured babel, react and server; displays index.html & components

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,92 @@
 
+// class CreateAccount extends React.Component{
+//   constructor(props){
+//     super(props);
+//   }
+//   render(){
+//     return (
+//       <div id='UserAccForm'>
+//         <form method='post' action='/createUser'>
+//           <label> Name:
+//             <input type='text'/>
+//           </label>
+//           <label> Email:
+//             <input type='text'/>
+//           </label>
+//           <label> Password:
+//             <input type='text'/>
+//           </label>
+//         </form>
+//         <button onclick={this.props.buttonClick}>Next</button>
+//       </div>
+//     );
+//   }
+// }
+function CreateAccount(props) { 
+  console.log('createAccount');
+  return (
+    <div id='UserAccForm'>
+      <form method='POST' action='/createUser'>
+        <label> Name:
+          <input type='text'/>
+        </label>
+        <label> Email:
+          <input type='text'/>
+        </label>
+        <label> Password:
+          <input type='text'/>
+        </label>
+      </form>
+      <button onClick={()=>{props.buttonClick}}>Next</button>
+    </div>
+    );
+}
 
+function HomePage(props) { 
+  return (
+    <div id='home'>
+      <button onClick={()=>{props.onClick}}>Checkout</button> 
+    </div>
+  );
+}
+
+class LoadPage extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      form : 'homePage',
+      page: <HomePage onClick={()=>{this.buttonClick}}/> 
+    };
+    this.buttonClick = this.buttonClick.bind(this);
+  }
+
+  buttonClick(event){
+    console.log('click',event);
+    if(this.form === 'homePage') {
+      this.setState(state =>({form: 'accountUser', page: <CreateAccount /> }));
+    } else if(this.form === 'userAccount') {
+      this.setState(state =>({form: 'accountUser'}));
+    } else if (this.form === 'userShipping') {
+      this.setState(state =>({form: 'accountUser'}));
+    } else if (this.form === 'userBilling') {
+      this.setState(state =>({form: 'accountUser'}));
+    } else if(this.form === 'userConfirm') {
+      this.setState(state =>({form: 'accountUser'}));
+    } else {
+      this.setState(state =>({form: 'accountUser'}));
+    }
+  }
+
+  // componentDidMount(){
+  //   console.log('home page,',this.buttonClick);
+  //   this.setState(state =>({form: 'home', page: <HomePage click={()=>{this.buttonClick}}/> }));
+  // }
+
+  render(){
+    console.log('REndering: ',this.state.page);
+    return this.state.page;
+  }
+}
+
+
+ReactDOM.render(<LoadPage />, document.getElementById('shoppingCart')); //??
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,18 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+var port = 3000;
+ app.use(express.static(__dirname + "/public"));  //sets up index.html on localhost
+
+app.post('/createUser', function(req, res){
+    console.log('creating user');
+    res.end();
+});
+
+app.listen(port, function(){
+    console.log(`Listening on port ${port}!`);
+});
